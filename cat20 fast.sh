a2a41626bb297497162fcf405d66e87abca8d469
Amberiscoolguy <133490948+Amberiscoolguy@users.noreply.github.com>
2025-09-30 10:47:13 +0800

    Update cat20 fast.sh

--- cat20 fast.sh
+++ cat20 fast.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+
 while true; do
     fastestFee="null" 
 
     while [ -z "$fastestFee" ] || [ "$fastestFee" == "null" ]; do
         response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/mempool-blocks)
-        fastestFee=$(echo $response | jq '.[0].feeRange | .[-3]') 
-        echo "獲取fastestFee: $fastestFee"
 
+        fastestFee=$(echo $response | jq '[.[] | .medianFee] | max')
+        echo "獲取medianFee: $fastestFee"
 
         if [ -z "$fastestFee" ] || [ "$fastestFee" == "null" ]; then
-            echo "未獲取fastestFee，retrying"
+            echo "獲取medianFee失敗，重新嘗試"
             sleep 1
         fi
     done
 
-    echo "有效的 fastestFee: $fastestFee"
+    echo "有效medianFee: $fastestFee"
 
     command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --fee-rate $fastestFee"
     $command
@@ -22,6 +24,22 @@ while true; do
         echo "mint失敗，腳本暫停"
         exit 1
     fi
+
+    sleep 20
+    while true; do
+        response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/mempool-blocks)
+
+        minFeeInRange=$(echo $response | jq '.[0].feeRange | min')
+        echo "最新塊最低值: $minFeeInRange"
+
+        if (( $(echo "$minFeeInRange < $fastestFee" | bc -l) )); then
+            echo "確認過塊，繼續執行..."
+            break
+        else
+            echo "未過塊，等待中..."
+            sleep 5
+        fi
+    done
 
     sleep 1
 done
